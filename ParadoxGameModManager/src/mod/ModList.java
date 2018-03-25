be5e9a8a22cde46e92d018b5820dc969966d1477
NicolasGrosjean <grosjen@hotmail.fr>
2018-03-25 17:45:05 +0200

    Implements ModList conflicts #18
    Also fix a bug with ModList#isModInList

--- ParadoxGameModManager/src/mod/ModList.java
+++ ParadoxGameModManager/src/mod/ModList.java
@@ -1,6 +1,7 @@
 package mod;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,8 @@ public class ModList {
 	private Languages language;
 	private List<Mod> modlist;
 	private List<ModConflict> modConflicts;
+	
+	private static int MOD_NOT_IN_LIST = -1;
 
 	/**
 	 * @param name
@@ -112,7 +115,7 @@ public class ModList {
 			if(one_mod.equals(mod))
 				return i;
 		}
-		return 0;
+		return MOD_NOT_IN_LIST;
 	}
 	
 	/**
@@ -120,7 +123,7 @@ public class ModList {
 	 * @return
 	 */
 	public boolean addMod(Mod mod){
-		if(isModInList(mod)==0){
+		if(isModInList(mod) == MOD_NOT_IN_LIST){
 			modlist.add(mod);
 			addConflicts(mod);
 			return true;
@@ -143,26 +146,63 @@ public class ModList {
 	 */
 	public boolean removeMod(Mod mod){
 		int index = isModInList(mod);
-		if(index!=0){
+		if(index!=MOD_NOT_IN_LIST){
 			modlist.remove(index);
 			removeConflicts(mod);
 		}
 		return false;
 	}
 	
-	private void computeConflicts(List<Mod> modList)
-	{
+	private void computeConflicts(List<Mod> modList) {
 		this.modConflicts = new ArrayList<>();
-		// TODO
+		for (Mod mod1 : modList) {
+			Iterator<Mod> it = modList.iterator();
+			boolean mod1Found = false;
+			while (it.hasNext()) {
+				// We skip mod1 and all the previous mods because they have already been seen
+				Mod mod2 = it.next();
+				if (mod2.equals(mod1)) {
+					mod1Found = true;
+					continue;
+				}
+				if (!mod1Found) {
+					continue;
+				}
+
+				// Search conflict
+				addModsConflict(mod1, mod2);
+			}
+		}
 	}
-	
-	private void addConflicts(Mod newMod)
-	{
-		// TODO
+
+	private void addConflicts(Mod newMod) {
+		for (Mod mod1 : modlist) {
+			if (!mod1.equals(newMod)) {
+				addModsConflict(mod1, newMod);
+			}
+		}
 	}
-	
-	private void removeConflicts(Mod removedMod)
-	{
-		// TODO
+
+	private void removeConflicts(Mod removedMod) {
+		Iterator<ModConflict> it = modConflicts.iterator();
+		while (it.hasNext()) {
+			ModConflict conflict = it.next();
+			if (conflict.getMod1().equals(removedMod) ||
+					conflict.getMod2().equals(removedMod)) {
+				it.remove();
+			}
+		}
+	}
+
+	private void addModsConflict(Mod mod1, Mod mod2) {
+		ModConflict modConflict = new ModConflict(mod1, mod2);
+		for (String modifiedFile : mod1.getModifiedFiles()) {
+			if (mod2.getModifiedFiles().contains(modifiedFile)) {
+				modConflict.addConflictFile(modifiedFile);
+			}
+		}
+		if (modConflict.getConflictFiles().size() > 0) {
+			modConflicts.add(modConflict);
+		}
 	}
 }
