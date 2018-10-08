3ea6105ce534cec4020052eda5c4abe292742e16
ThibautSF <tsimonfine@gmail.com>
2018-10-08 14:58:00 +0200

    fix scripts and missing doc

--- build/dist/LaunchUnix.sh
+++ build/dist/LaunchUnix.sh
@@ -2,4 +2,4 @@
 
 _CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$_CURRENT_FILE_DIR"
-java -jar "$_CURRENT_FILE_DIR"/ParadoxosModManager.jar
+java -jar "$_CURRENT_FILE_DIR/ParadoxosModManager.jar"
--- build/dist/LaunchWindows.cmd
+++ build/dist/LaunchWindows.cmd
@@ -2,4 +2,4 @@ set _CURRENT_FILE_DIR=%~dp0
 set _CURRENT_FILE_DIR=%_CURRENT_FILE_DIR:~0,-1%
 
 cd /D %_CURRENT_FILE_DIR%
-java -jar %_CURRENT_FILE_DIR%\ParadoxosModManager.jar
+java -jar "%_CURRENT_FILE_DIR%\ParadoxosModManager.jar"
